d8b3ca10db523062e827480cc4d179cb51caf955
haoChenGIS <ch2662353032@gmail.com>
2025-04-22 12:22:26 +0800

    Add a title

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import TourCard from "./components/TourCard";
 import { Container } from "@mui/material";
 import Grid from "@mui/material/Grid";
+
+import { Typography, Box } from "@mui/material";
 function App() {
   return (
     <div>
       <Container>
+        <Box sx={{ textAlign: "center", marginBottom: 2 }}>
+          <Typography variant="h2" component="h1" gutterBottom>
+            Tour Cards
+          </Typography>
+        </Box>
         <Grid container spacing={2}>
           <TourCard />
           <TourCard />
