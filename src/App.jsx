18c0c0c384be07db93732d0029a58c7c3b8d6225
haoChenGIS <ch2662353032@gmail.com>
2025-04-23 11:02:17 +0800

    Finish the first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
+import React from "react";
 import TourCard from "./components/TourCard";
-import { Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import SearchAppBar from "./components/SearchAppBar";
+import cities from "./data.json";
 
-import { Typography, Box } from "@mui/material";
 function App() {
   return (
     <div>
-      <Container>
-        <Box sx={{ textAlign: "center", marginBottom: 2 }}>
-          <Typography variant="h2" component="h1" gutterBottom>
-            Tour Cards
-          </Typography>
-        </Box>
-        <Grid container spacing={2}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+      <SearchAppBar />
+      <Container sx={{ mt: 2 }}>
+        {cities.map((city) => (
+          <React.Fragment key={city.id}>
+            <Typography variant="h4" component="h2" mt={5} mb={3}>
+              Top {city.name}
+            </Typography>
+            <Grid container spacing={2}>
+              {city.tours.map((tour) => (
+                <TourCard key={tour.id} tour={tour} />
+              ))}
+            </Grid>
+          </React.Fragment>
+        ))}
       </Container>
     </div>
   );
--- src/components/TourCard.jsx
+++ src/components/TourCard.jsx
@@ -2,61 +2,105 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Item from "@mui/material/Grid";
 import { Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
+
 import Box from "@mui/material/Box";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import Rating from "@mui/material/Rating";
 
-export default function TourCard() {
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: { variant: "body2" },
+          style: {
+            fontSize: "0.6rem",
+          },
+        },
+        {
+          props: { variant: "h6" },
+          style: {
+            fontSize: "0.8rem",
+          },
+        },
+      ],
+    },
+  },
+});
+
+export default function TourCard({ tour }) {
   return (
-    <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }}>
-      <Item>
-        <Paper elevation={3}>
-          <Box
-            component="img"
-            sx={{
-              width: "100%",
-              height: "auto",
-              borderRadius: 1, // 添加圆角
-            }}
-            src="https://plus.unsplash.com/premium_photo-1669863283335-9370289707d7?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZmFsbHN8ZW58MHx8MHx8fDA%3D"
-            alt="pic"
-          />
-          <Box sx={{ paddingX: 2 }}>
-            <Typography variant="subtitle1" component="h2">
-              Immerse into the Falls
-            </Typography>
+    //size={{ xs: 12, sm: 6, md: 4, lg: 3 }}
+    <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3 }} item>
+      <ThemeProvider theme={theme}>
+        <Item>
+          <Paper elevation={3} sx={{ width: "100%", height: "18rem" }}>
             <Box
+              component="img"
               sx={{
-                display: "flex",
-                alignItems: "center",
+                width: "100%",
+                height: "10rem",
+                borderRadius: 1, // 添加圆角
               }}
-            >
-              <AccessTimeIcon
-                sx={{
-                  width: 12.5,
-                }}
-              />
-              <Typography variant="body2" component="p" marginLeft={0.5}>
-                5 hours
+              src={tour.image}
+              alt="pic"
+            />
+            <Box sx={{ paddingX: 2 }}>
+              <Typography variant="subtitle1" component="h2">
+                {tour.name}
               </Typography>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <AccessTimeIcon
+                  sx={{
+                    width: 12.5,
+                  }}
+                />
+                <Typography variant="body2" component="p" marginLeft={0.5}>
+                  {tour.duration} hours
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: 1,
+                  justifyContent: "space-between",
+                }}
+              >
+                <Rating
+                  name="read-only"
+                  value={tour.rating}
+                  precision={0.5}
+                  size="small"
+                  readOnly
+                />
+                <Typography variant="body2" component="p" marginLeft={0.5}>
+                  {tour.rating} points
+                </Typography>
+                <Typography variant="body2" component="p" marginLeft={0.5}>
+                  ({tour.numberOfReviews} reviews)
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  variant="h6"
+                  component="h3"
+                  marginTop="0.5rem"
+                  sx={{ fontWeight: "bold" }}
+                >
+                  From C ${tour.price} Dallers
+                </Typography>
+              </Box>
             </Box>
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <Rating
-                name="read-only"
-                value={4.5}
-                precision={0.5}
-                size="small"
-                readOnly
-              />
-            </Box>
-          </Box>
-        </Paper>
-      </Item>
+          </Paper>
+        </Item>
+      </ThemeProvider>
     </Grid>
   );
 }
